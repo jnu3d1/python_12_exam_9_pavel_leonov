59c887e9d206cba966f4effd719d9f015c0b968a
jnu3d1 <jnu3d1@rambler.ru>
2022-09-24 18:52:43 +0600

    Этап 4 Изменения, которые включены в коммит: 	новый файл:    fixtures/auth.json 	новый файл:    fixtures/dump.json 	изменено:      webapp/templates/picture.html 	изменено:      webapp/views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -8,19 +9,19 @@ from webapp.models import Picture, Album
 
 # Create your views here.
 
-class PicturesView(ListView):
+class PicturesView(LoginRequiredMixin, ListView):
     model = Picture
     template_name = 'all_pictures.html'
     context_object_name = 'pictures'
     ordering = ('-created_at',)
 
 
-class PictureView(DetailView):
+class PictureView(LoginRequiredMixin, DetailView):
     model = Picture
     template_name = 'picture.html'
 
 
-class UploadPictureView(CreateView):
+class UploadPictureView(LoginRequiredMixin, CreateView):
     form_class = PictureForm
     template_name = 'upload_picture.html'
 
@@ -33,24 +34,32 @@ class UploadPictureView(CreateView):
         return reverse('webapp:pictures')
 
 
-class UpdatePictureView(UpdateView):
+class UpdatePictureView(PermissionRequiredMixin, UpdateView):
     form_class = PictureForm
     model = Picture
+    permission_required = 'webapp.change_picture'
     template_name = 'update_picture.html'
+
+    def has_permission(self):
+        return super().has_permission() or self.get_object().author == self.request.user
 
     def get_success_url(self):
         return reverse('webapp:picture', kwargs={'pk': self.object.pk})
 
 
-class DeletePictureView(DeleteView):
+class DeletePictureView(PermissionRequiredMixin, DeleteView):
     model = Picture
+    permission_required = 'webapp.delete_picture'
     template_name = 'delete_picture.html'
+
+    def has_permission(self):
+        return super().has_permission() or self.get_object().author == self.request.user
 
     def get_success_url(self):
         return reverse('webapp:pictures')
 
 
-class AlbumView(DetailView):
+class AlbumView(LoginRequiredMixin, DetailView):
     model = Album
     template_name = 'album.html'
 
@@ -61,7 +70,7 @@ class AlbumView(DetailView):
         return context
 
 
-class CreateAlbumView(CreateView):
+class CreateAlbumView(LoginRequiredMixin, CreateView):
     form_class = AlbumForm
     template_name = 'create_album.html'
 
@@ -74,18 +83,26 @@ class CreateAlbumView(CreateView):
         return reverse('webapp:pictures')
 
 
-class UpdateAlbumView(UpdateView):
+class UpdateAlbumView(PermissionRequiredMixin, UpdateView):
     form_class = AlbumForm
     model = Album
+    permission_required = 'webapp.change_album'
     template_name = 'update_album.html'
+
+    def has_permission(self):
+        return super().has_permission() or self.get_object().author == self.request.user
 
     def get_success_url(self):
         return reverse('webapp:album', kwargs={'pk': self.object.pk})
 
 
-class DeleteAlbumView(DeleteView):
+class DeleteAlbumView(PermissionRequiredMixin, DeleteView):
     model = Album
+    permission_required = 'webapp.delete_album'
     template_name = 'delete_album.html'
+
+    def has_permission(self):
+        return super().has_permission() or self.get_object().author == self.request.user
 
     def get_success_url(self):
         return reverse('webapp:pictures')
